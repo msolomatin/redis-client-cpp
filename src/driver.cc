60d6a69d24c917c11cb2dc82d18c7e6c07f3c87d
Mikhail Solomatin <m.solomatin@gmail.com>
2015-09-25 23:09:55 +0100

    Added driver program

---
+++ src/driver.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Redis.h"
+
+int main() {
+  
+  Redis r("localhost", 6379, "");
+  auto value = r.ping("preved");
+  std::cout << value << std::endl;
+  r.set("hello world");
+  std::cout << r.get("hello") << std::endl;
+  r.quit();
+
+  return 0;
+}
